4fce6dea838eaff204045e10e15dd70f64c24c7f
LJQ0526 <49089688+LJQ0526@users.noreply.github.com>
2021-01-30 23:58:35 +0800

    echarts
    示例图

--- src/pages/View/index.tsx
+++ src/pages/View/index.tsx
@@ -1,21 +1,12 @@
 import CanvasSample from '@/components/view/EchartTest';
-import { createElement, useEffect } from 'rax';
+import { createElement } from 'rax';
 import View from 'rax-view';
 
 
-
 export default function Login() {
- 
-
-  return(
-    <>
-  <view>我是可视化页</view>
-  <CanvasSample />
-
-  
- </>
-    ) ;
- 
-
-  
+  return (
+    <View>
+      <CanvasSample />
+    </View>
+  );
 }
