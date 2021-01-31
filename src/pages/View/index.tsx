72d1570134935bac95b2ad72385e5f708dc10eca
zh-D <1641245614@qq.com>
2021-01-31 22:38:35 +0800

    del

--- src/pages/View/index.tsx
+++ src/pages/View/index.tsx
@@ -1,12 +1,10 @@
-import CanvasSample from '@/components/view/EchartTest';
 import { createElement } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
 import GroupHistograms from '../../components/View/GroupHistograms';
 import styles from './index.module.css';
 import EchartDate from '../../components/View/EchartDate';
-import HeatMap from '../../components/View/HeatMap'
-
+import HeatMap from '../../components/View/HeatMap';
 
 export default function Login() {
   return (
